3133ad58c0190ad1e4baa1fb812b834726232eda
santhosh4456 <100783089+santhosh4456@users.noreply.github.com>
2022-03-28 12:23:11 +0530

    Create sum_func_asign3.py

---
+++ sum_func_asign3.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+def sum(numbers):
+    count = 0
+    for x in numbers:
+        count += x
+    return count
+print(sum((8,2,3,7)))
+
+
+# In[ ]:
+
+
+
+
